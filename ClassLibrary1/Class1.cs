bfa86a1706ee908ffdb03aee589ce988a113aa24
SaZyGH <103922719+SaZyGH@users.noreply.github.com>
2023-03-06 10:22:31 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class User
+    {
+        public string Login { get; }
+
+        public User(string login)
+        {
+            Login = login;
+        }
+        public static User LogIn(string login, string password)
+        {
+            return login == "admin" && password == "123" ? new User(login): null;
+        }
+    }
+}
